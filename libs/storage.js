4e48a327e8b390a82e4cdf5acff5d93867d98790
Michael Knepprath <mknepprath@Enoch.attlocal.net>
2020-05-22 08:52:01 -0400

    Add jsdoc to storage.js

--- libs/storage.js
+++ libs/storage.js
@@ -1,5 +1,14 @@
+/**
+ * Gets an item in localStorage.
+ * @param {string} key The localStorage key for the item being fetched.
+ */
 export const getStoredItem = (key) =>
   localStorage && JSON.parse(localStorage.getItem(key));
 
+/**
+ * Adds an item to localStorage.
+ * @param {string} key The localStorage key for the item being fetched.
+ * @param {string} value The value being stored to the specified key.
+ */
 export const setStoredItem = (key, value) =>
   localStorage && localStorage.setItem(key, JSON.stringify(value));
