de92be26de9815afcfceda96d6994aee71801cae
Michael Knepprath <mknepprath@Enoch.attlocal.net>
2020-05-29 16:14:45 -0400

    Add messy 404 page

---
+++ pages/404.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import randomInteger from "../libs/random-integer";
+import useSWR from "swr";
+
+import fetcher from "../libs/fetch";
+import styles from "./404.module.css";
+
+export default function Custom404() {
+  const { data: recommendations, error } = useSWR(
+    "/api/recommendations",
+    fetcher
+  );
+
+  let rec;
+
+  if (recommendations) {
+    const index = randomInteger(0, recommendations.length);
+    rec = recommendations[index];
+  }
+
+  return (
+    <>
+      <Link href="/">
+        <img
+          alt="A photo of a broken robot, signifying the fact that you've reached a error page."
+          className={styles.error}
+          src="/error.jpg"
+        />
+      </Link>
+
+      <div className={styles.content}>
+        <h1>404 - Page Not Found</h1>
+        {rec ? (
+          <p>
+            Have you checked out <a href={rec.url}>{rec.recommendation}</a> yet?
+          </p>
+        ) : (
+          <p>...</p>
+        )}
+      </div>
+    </>
+  );
+}
