8ac83993d5b10e0778aa13d74805d7f67707d545
Michael Knepprath <mknepprath@gmail.com>
2021-08-08 12:48:21 -0400

    Fix Slack notif

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -7,29 +7,40 @@ Airtable.configure({
 
 export default async (req, res) => {
   const body = JSON.parse(req.body);
-  
-  await fetch(`https://hooks.slack.com/services/T0259ER7T/B01HV93RB5Z/${process.env.SLACK_TOKEN}`, {
-    method: "POST",
-    body: JSON.stringify({
-      text: `${body.name} recommended ${body.recommendation}`,
-      blocks: [{
-        type: "section",
-        text: {
-          type: "plain_text",
-          text: `${body.name} recommended ${body.recommendation}. Thanks, ${["bub", "pal", "friend", "really"][Math.floor(Math.random() * 4)]}!`
-        }
-      },
-      {
-        type: "section",
-        text: {
-          type: "plain_text",
-          text: `They said, \"${body.message}\"`
-        }
-      }
-    ]
-    })
-  })
 
-  res.status(200)
-  res.end()
+  const blocks = [
+    {
+      type: "section",
+      text: {
+        type: "plain_text",
+        text: `${body.name} recommended ${body.recommendation}. Thanks, ${
+          ["bub", "pal", "friend", "really"][Math.floor(Math.random() * 4)]
+        }!`,
+      },
+    },
+  ];
+
+  if (body.message !== "") {
+    blocks.push({
+      type: "section",
+      text: {
+        type: "plain_text",
+        text: `They said, \"${body.message}\"`,
+      },
+    });
+  }
+
+  await fetch(
+    `https://hooks.slack.com/services/T0259ER7T/B01HV93RB5Z/${process.env.SLACK_TOKEN}`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        text: `${body.name} recommended ${body.recommendation}`,
+        blocks,
+      }),
+    }
+  );
+
+  res.status(200);
+  res.end();
 };
