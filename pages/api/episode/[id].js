99f4d3865261545933ff3c1ec26245b794696aa9
Michael Knepprath <mknepprath@gmail.com>
2022-02-13 18:01:41 -0500

    Use S3 URL

--- pages/api/episode/[id].js
+++ pages/api/episode/[id].js
@@ -3,7 +3,7 @@ import { EPISODES } from "../../../libs/constants";
 export default ({ query: { id } }, res) => {
   res.status(200).json({
     audio: {
-      url: `${process.env.NEXT_PUBLIC_S3_BUCKET}/${id}.mp3`,
+      url: `https://culturallyirrelevant.s3.us-east-2.amazonaws.com/episodes/${id}.mp3`,
     },
     episode: `${EPISODES[id - 1].episode}`,
     name: `${EPISODES[id - 1].title}`,
