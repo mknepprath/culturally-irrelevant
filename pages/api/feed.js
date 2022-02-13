342be1dbf7dae2cbde65ac3cf6116f7648abb52d
Michael Knepprath <mknepprath@gmail.com>
2022-02-13 16:56:38 -0500

    More feed massaging

--- pages/api/feed.js
+++ pages/api/feed.js
@@ -1,8 +1,7 @@
 import { EPISODES } from "../../libs/constants";
+// process.env.NEXT_PUBLIC_S3_BUCKET
 
 export default (_, res) => {
-  // console.log(`${process.env.NEXT_PUBLIC_S3_BUCKET}/${id}.mp3`);
-
   res.setHeader("Content-Type", "text/xml");
   res.write(`<?xml version="1.0" encoding="UTF-8"?>
   <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:content="http://purl.org/rss/1.0/modules/content/">
@@ -24,31 +23,23 @@ export default (_, res) => {
   <itunes:category text="Books"/> </itunes:category>
   <itunes:explicit>false</itunes:explicit>
   ${EPISODES.map(
-    (data, index) => `<item>
-  <title>#${data.episode} &#8211; ${data.title}</title>
-  <link>https://www.culturallyirrelevant.com/</link>
+    (data, index) => `
+    <item>
+      <title>#${data.episode} &#8211; ${data.title}</title>
+      <link>https://www.culturallyirrelevant.com/</link>
 
-  <dc:creator><![CDATA[flophouse]]></dc:creator>
-  <pubDate>${data.publish_date}</pubDate>
+      <dc:creator><![CDATA[culturallyirrelevant]]></dc:creator>
+      <pubDate>${data.publish_date}</pubDate>
       <category><![CDATA[Episodes]]></category>
-  <category><![CDATA[Adam Rex]]></category>
-  <category><![CDATA[Dan McCoy]]></category>
-  <category><![CDATA[Dungeons & Dragons]]></category>
-  <category><![CDATA[Elliott Kalan]]></category>
-  <category><![CDATA[Jeremy Irons]]></category>
-  <category><![CDATA[Justin Whalen]]></category>
-  <category><![CDATA[Marlon Wayans]]></category>
-  <category><![CDATA[Stuart Wellington]]></category>
-  <category><![CDATA[Thora Birch]]></category>
-  <category><![CDATA[Zoe McClellan]]></category>
-  <guid isPermaLink="false">https://www.flophousepodcast.com/?p=12841</guid>
+      <guid isPermaLink="false">https://www.culturallyirrelevant.com/episode/${
+        index + 1
+      }</guid>
 
-        <description><![CDATA[Episode ${data.episode}...]]></description>
-                  <content:encoded><![CDATA[<p>Encoded content</p>]]></content:encoded>
-  <enclosure url="https://culturallyirrelevant.s3.us-east-2.amazonaws.com/episodes/${
-    index + 1
-  }.mp3" length="131321241" type="audio/mpeg" />
-
+      <description><![CDATA[Episode ${data.episode}...]]></description>
+      <content:encoded><![CDATA[<p>Encoded content</p>]]></content:encoded>
+      <enclosure url="https://culturallyirrelevant.s3.us-east-2.amazonaws.com/episodes/${
+        index + 1
+      }.mp3" length="131321241" type="audio/mpeg" />
     </item>`
   )}
   </channel>
